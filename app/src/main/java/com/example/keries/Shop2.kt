21f47b3d9820f02d7956b162a5b02167a25b6f64
ishant <ishantkuwt@gmail.com>
2023-09-19 00:56:52 +0530

    commit 8 ishant

---
+++ app/src/main/java/com/example/keries/Shop2.kt
@@ -0,0 +1,35 @@
+package com.example.keries
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Shop2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_shop2, container, false)
+
+
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+
+
+
+
+
+}
