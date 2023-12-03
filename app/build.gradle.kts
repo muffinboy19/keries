e33f70261ead18f871b247b6b7ac405d2424eb48
Naitik <iib2023036@iiita.ac.in>
2023-12-03 16:41:33 +0530

    commit 2 naitik

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation("com.google.firebase:firebase-messaging-ktx:23.2.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+    implementation("androidx.core:core-animation:1.0.0-rc01")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.squareup.picasso:picasso:2.71828")
--- app/src/main/java/com/example/keries/activity/Splashscreen.kt
+++ app/src/main/java/com/example/keries/activity/Splashscreen.kt
@@ -2,28 +2,48 @@ package com.example.keries.activity
 
 import android.content.Intent
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.keries.R
+import com.example.keries.activity.BaseHome
+import android.animation.ObjectAnimator
+import android.view.animation.AccelerateDecelerateInterpolator
 
 class splashscreen : AppCompatActivity() {
     private val SPLASH_DELAY = 2000L // 2 seconds
-    private var mediaPlayer: MediaPlayer?=null
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
-            // Using a Handler to delay the transition to the main activity
+
+        val logoview: ImageView = findViewById(R.id.logo)
+        logoview.alpha = 0f
+
+        // Fade in animation for the logo using ObjectAnimator
+        val fadeInAnimator = ObjectAnimator.ofFloat(logoview, "alpha", 0f, 1f)
+        fadeInAnimator.duration = 1000 // 1 second
+        fadeInAnimator.interpolator = AccelerateDecelerateInterpolator()
+        fadeInAnimator.start()
 
         // Initialize the MediaPlayer with the audio file
         mediaPlayer = MediaPlayer.create(this, R.raw.music)
 
-            Handler().postDelayed({
-                val mainIntent = Intent(this,BaseHome::class.java)
-                startActivity(mainIntent)
-                finish()
-            }, SPLASH_DELAY)
-        }
+        // Start playing the audio
+        mediaPlayer?.start()
+
+        // Using a Handler to delay the transition to the main activity
+        Handler().postDelayed({
+            val mainIntent = Intent(this, BaseHome::class.java)
+
+            // Adding transition animation between activities
+            startActivity(mainIntent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }, SPLASH_DELAY)
+    }
 
     override fun onDestroy() {
         super.onDestroy()
